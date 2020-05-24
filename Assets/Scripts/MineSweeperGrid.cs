3bdf737501499978350a740ede45a0a495bd3268
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-05-24 14:20:03 +0200

    Extend the grid to generate 2D and 3D grids

--- Assets/Scripts/MineSweeperGrid.cs
+++ Assets/Scripts/MineSweeperGrid.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MineSweeperGrid2D : MonoBehaviour
+public class MineSweeperGrid : MonoBehaviour
 {
 	[SerializeField] GameObject cellPrefab;
 	[SerializeField] int width;
 	[SerializeField] int height;
+	[SerializeField] int depth;
 	[SerializeField] float cellSpacing;
 
 	private List<GameObject> cells = new List<GameObject>();
@@ -16,17 +17,21 @@ public class MineSweeperGrid2D : MonoBehaviour
     {
 		float cellWidth = cellPrefab.transform.localScale.x;
 		float cellHeight = cellPrefab.transform.localScale.y;
+		float cellDepth = cellPrefab.transform.localScale.z;
 		float startX = transform.position.x - (cellWidth * width / 2);
 		float startY = transform.position.y - (cellHeight * height / 2);
+		float startZ = transform.position.z - (cellDepth * depth);
         for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
-				GameObject cell = Instantiate(cellPrefab, transform.position, Quaternion.identity);
-				cell.transform.position = new Vector3(
-					startX + i * (cellWidth + cellSpacing),
-					startY + j * (cellHeight + cellSpacing),
-					transform.position.z
-				);
-				cells.Add(cell);
+				for (int k = 0; k < depth; k++) {
+					GameObject cell = Instantiate(cellPrefab, transform.position, Quaternion.identity);
+					cell.transform.position = new Vector3(
+						startX + i * (cellWidth + cellSpacing),
+						startY + j * (cellHeight + cellSpacing),
+						startZ + k * (cellDepth + cellSpacing)
+					);
+					cells.Add(cell);
+				}
 			}
 		}
     }
