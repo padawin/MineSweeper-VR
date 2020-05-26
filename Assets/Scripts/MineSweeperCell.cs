1a16786e807b0fb438e2f36327d9b08667419fac
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-05-26 21:58:36 +0200

    Basic hands control added

---
+++ Assets/Scripts/MineSweeperCell.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineSweeperCell : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+		Debug.Log(other.name);
+    }
+}
---
+++ Assets/Scripts/MineSweeperDestroyCell.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineSweeperDestroyCell : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other) {
+		Debug.Log(other.name);
+		Destroy(other.gameObject);
+	}
+}
