e3fb525847e2a44092a3e213ab182c90773aeb21
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-05-31 14:43:58 +0200

    Translation done relative to the player

--- Assets/Scripts/MineSweeperTranslateGrid.cs
+++ Assets/Scripts/MineSweeperTranslateGrid.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 public class MineSweeperTranslateGrid : MonoBehaviour
 {
-	[SerializeField] MineSweeperGrid grid;
-	// Negative speed to reverse the rotation
-	[SerializeField] float translationSpeed = 5.0f;
+	[SerializeField] float translationSpeed = 1.0f;
+	[SerializeField] float minDistanceFromPlayer = 0.5f;
 
-    void Update() {
+	GameObject grid;
+	GameObject player;
+
+	private void Start() {
+		grid = GameObject.FindGameObjectWithTag("grid");
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update() {
 		Vector2 sideTranslate = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.LTouch);
-		float depthTranslate = 0.0f;
+		Vector3 towardsPlayer = player.transform.position - grid.transform.position;
+		Vector3 up = new Vector3(0.0f, 1.0f, 0.0f);
+		Vector3 side = Vector3.Cross(towardsPlayer, up);
+		float speedTowardsPlayer;
 		if (OVRInput.Get(OVRInput.RawButton.Y)) {
-			depthTranslate = translationSpeed;
+			speedTowardsPlayer = translationSpeed; // we push the grid away
 		}
-		else if (OVRInput.Get(OVRInput.RawButton.X)) {
-			depthTranslate = -translationSpeed;
+		else if (OVRInput.Get(OVRInput.RawButton.X) && towardsPlayer.magnitude > minDistanceFromPlayer) {
+			speedTowardsPlayer = -translationSpeed; // we pull the grid closer
 		}
-		grid.gameObject.transform.Translate(new Vector3(
-			sideTranslate.x * translationSpeed * Time.deltaTime,
-			sideTranslate.y * translationSpeed * Time.deltaTime,
-			depthTranslate * Time.deltaTime
-		));
-    }
+		else {
+			speedTowardsPlayer = 0; // we don't move the grid
+		}
+		towardsPlayer.Normalize();
+		towardsPlayer = towardsPlayer * -speedTowardsPlayer;
+		grid.transform.position = grid.transform.position
+			+ towardsPlayer * Time.deltaTime
+			+ up * sideTranslate.y * translationSpeed * Time.deltaTime
+			+ side * sideTranslate.x * translationSpeed * Time.deltaTime;
+	}
 }
