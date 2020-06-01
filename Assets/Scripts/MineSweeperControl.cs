2d4032d555e9889e423313f14a70ae238f676ed6
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-06-01 15:05:50 +0200

    Extract Cells hovering and controller vibration is separate script

---
+++ Assets/Scripts/MineSweeperControl.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineSweeperControl : MonoBehaviour
+{
+	[SerializeField] float vibrationFrequency = 0.5f;
+	[SerializeField] float vibrationAmplitude = 0.5f;
+	List<MineSweeperCell> hoveredCells = new List<MineSweeperCell>();
+
+	void OnTriggerEnter(Collider other) {
+		MineSweeperCell cell = other.GetComponent<MineSweeperCell>();
+		cell.setHovered(true);
+		hoveredCells.Add(cell);
+	}
+
+	void OnTriggerExit(Collider other) {
+		MineSweeperCell cell = other.GetComponent<MineSweeperCell>();
+		cell.setHovered(false);
+		hoveredCells.Remove(cell);
+	}
+
+	public void reveal(MineSweeperGrid grid) {
+		void callback(MineSweeperCell cell) {
+			grid.revealCell(cell);
+		}
+		execute(callback);
+	}
+
+	public void mark() {
+		void callback(MineSweeperCell cell) {
+			cell.mark();
+		}
+		execute(callback);
+	}
+
+	public void execute(System.Action<MineSweeperCell> callback) {
+		foreach (var cell in hoveredCells) {
+			callback(cell);
+		}
+		if (hoveredCells.Count > 0) {
+			vibrate();
+		}
+		hoveredCells.Clear();
+	}
+
+	public List<MineSweeperCell> getHoveredCells() {
+		return hoveredCells;
+	}
+
+	public void vibrate() {
+		OVRInput.SetControllerVibration(vibrationFrequency, vibrationAmplitude, OVRInput.Controller.RTouch);
+		StartCoroutine("stopVibrating");
+	}
+
+	IEnumerator stopVibrating() {
+		yield return new WaitForSeconds(0.1f);
+		OVRInput.SetControllerVibration(0.0f, 0.0f, OVRInput.Controller.RTouch);
+
+	}
+}
--- Assets/Scripts/MineSweeperRevealCell.cs
+++ Assets/Scripts/MineSweeperRevealCell.cs
@@ -7,40 +7,16 @@ public class MineSweeperRevealCell : MonoBehaviour
 	[SerializeField] float vibrationFrequency = 0.5f;
 	[SerializeField] float vibrationAmplitude = 0.5f;
 	MineSweeperGrid grid;
-	List<MineSweeperCell> hoveredCells = new List<MineSweeperCell>();
+	MineSweeperControl playerControl;
 
 	private void Start() {
+		playerControl = GetComponent<MineSweeperControl>();
 		grid = GameObject.FindGameObjectWithTag("grid").GetComponent<MineSweeperGrid>();
-	}
-
-	void OnTriggerEnter(Collider other) {
-		MineSweeperCell cell = other.GetComponent<MineSweeperCell>();
-		cell.setHovered(true);
-		hoveredCells.Add(cell);
-	}
-
-	void OnTriggerExit(Collider other) {
-		MineSweeperCell cell = other.GetComponent<MineSweeperCell>();
-		cell.setHovered(false);
-		hoveredCells.Remove(cell);
 	}
 
 	private void Update() {
 		if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger, OVRInput.Controller.RTouch)) {
-			foreach (var cell in hoveredCells) {
-				grid.revealCell(cell);
-			}
-			if (hoveredCells.Count > 0) {
-				OVRInput.SetControllerVibration(vibrationFrequency, vibrationAmplitude, OVRInput.Controller.RTouch);
-				StartCoroutine("stopVibrating");
-			}
-			hoveredCells.Clear();
+			playerControl.reveal(grid);
 		}
-	}
-
-	IEnumerator stopVibrating() {
-		yield return new WaitForSeconds(0.1f);
-		OVRInput.SetControllerVibration(0.0f, 0.0f, OVRInput.Controller.RTouch);
-
 	}
 }
