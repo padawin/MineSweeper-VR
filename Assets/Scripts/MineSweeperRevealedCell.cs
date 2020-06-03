43f293b2c1dbfeb15fb70ef03a2b7a619dd8799b
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-06-03 12:19:05 +0200

    Neighbour numbers colored, yay

--- Assets/Scripts/MineSweeperRevealedCell.cs
+++ Assets/Scripts/MineSweeperRevealedCell.cs
@@ -6,6 +6,8 @@ public class MineSweeperRevealedCell : MonoBehaviour
 {
 	Camera m_camera;
 	[SerializeField] TextMesh text;
+	[SerializeField] MeshRenderer textRenderer;
+	[SerializeField] Color[] colors;
 
 	private void Start() {
 		m_camera = FindObjectOfType<Camera>();
@@ -13,6 +15,9 @@ public class MineSweeperRevealedCell : MonoBehaviour
 
 	public void setValue(int val) {
 		text.text = val.ToString();
+		if (val < colors.Length) {
+			textRenderer.materials[0].SetColor("_Color", colors[val]);
+		}
 	}
 
 	void Update() {
